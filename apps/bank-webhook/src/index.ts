894dabdf0e5cdd2769bb21dc6b889b23000b59f6
hemanth18 <hg510443@gmail.com>
2024-11-20 19:20:19 +0530

    Test 2.3

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -12,13 +12,13 @@ app.use(express.json());
 const depositLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 3, // Limit each IP to 3 requests per windowMs
-  message: "Too many requests, try again after 5 minutes",
+  message: "Too many requests, try again after 10 minutes",
   standardHeaders: true, 
   legacyHeaders: false, 
   handler: (req, res) => {
     res.status(429).json({
       success: false,
-      message: "Too many requests, try again after 5 minutes.",
+      message: "Too many requests, try again after 10 minutes.",
     });
   },
 });
